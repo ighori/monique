d75187c025e95703484d4321be043a3e05e672da
Artur Siekielski <artur.siekielski@vhex.net>
2017-10-10 18:48:53 +0200

    Version 1.2 in setup.py.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
         name = 'monique',
-        version = '1.1.1',
+        version = '1.2',
         packages = find_packages(exclude=['mqe.tests',]),
         install_requires = [
             'monique-tables',
